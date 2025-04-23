5d03fcabaddb9163586217923e8986f58362bd50
alfiy <alfi@cakap.com>
2025-04-23 14:39:51 +0700

    fix(test-2): resolve UI and utility issues
    - Updated favicon and browser title bar
    - Fixed image import for "wave.png" in `Hero.module.scss`
    - Added optional chaining to variable `elTop` in `scrollToSectionByIdUtils`

--- test-2/src/utils/scrollToSectionByIdUtils.ts
+++ test-2/src/utils/scrollToSectionByIdUtils.ts
@@ -1,7 +1,7 @@
 const scrollToSectionByIdUtils = (id: string) => {
     const element = document.getElementById(id);
     setTimeout(() => {
-        const elTop = element.getBoundingClientRect().top + window.pageYOffset;
+        const elTop = element?.getBoundingClientRect().top + window?.pageYOffset;
         const headerOffset = 80;
         window.scrollTo({ top: elTop - headerOffset, behavior: "smooth" });
         window?.history?.pushState(null, "", `#${id}`);
